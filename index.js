87606d0eec89963f140ad87840e244c1375932e6
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-07-27 15:49:21 +0100

    Refactor project routes to fix incorrect URL path

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 const postsRouter = require('./routes/posts.router')
-const authRouter = require('./routes/auth.router')
-
 app.use("/api/v1/posts", postsRouter)
+
+const authRouter = require('./routes/auth.router')
 app.use("/api/v1/auth", authRouter)
+
+const projectsRouter = require('./routes/projects.router')
+app.use("/api/v1/projects", projectsRouter)
 
 const PORT = process.env.PORT || 5000
 
