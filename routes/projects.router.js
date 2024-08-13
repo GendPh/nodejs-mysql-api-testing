a528ef6c53165de9dce3f24f28aa855ed52c390f
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-08-13 11:08:46 +0100

    Refactor project routes to fix incorrect URL path

--- routes/projects.router.js
+++ routes/projects.router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const projectController = require("../controller/projects.controller")
 
 router.get("/", projectController.getAll);
-router.get("/:name", postsController.getByName);
+router.get("/:name", projectController.getByName);
 /* router.get("/:id", postsController.getById)
 router.post("/", postsController.create)
 router.put("/:id", postsController.update)
